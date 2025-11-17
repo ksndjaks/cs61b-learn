c9f3450a7d13ba2e8b88cc046af44d275eec7520
ksndjaks <ksndjaks@qq.com>
2025-11-18 00:05:07 +0800

    lab2 completed

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,36 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesAll() {
+        IntList lst = IntList.of(2, 3, 5, 7, 11, 13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121 -> 169", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquareNoPrimes() {
+        IntList lst = IntList.of(10, 20, 30, 40, 50);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("10 -> 20 -> 30 -> 40 -> 50", lst.toString());
+        assertTrue(!changed);
+    }
+
+    @Test
+    public void testSquarePrimesAtFirst() {
+        IntList lst = IntList.of(5, 10, 20, 30);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("25 -> 10 -> 20 -> 30", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesAtLast() {
+        IntList lst = IntList.of(10, 20, 30, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("10 -> 20 -> 30 -> 25", lst.toString());
+        assertTrue(changed);
+    }
 }
